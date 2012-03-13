5af0012f0e91ab0545f4c01ed7003ca4056b6bcf
dragonmen <dragonmen@gmail.com>
2012-03-13 14:55:16 +0000

    set headers and return correct info

--- stream_local.php
+++ stream_local.php
@@ -1,21 +1,29 @@
 <?php
 
 $filename=$_REQUEST["filename"];
+
 $fh=fopen($filename,"rb");
 if (!$fh)
     die("Cannot open <strong>{$filename}</strong>!");
 
-$hs=unserialize('a:4:{i:0;s:25:"Content-Type: video/x-flv";i:1;s:24:"Content-Length: ";i:2;s:44:"Last-Modified: Wed, 22 Jun 2011 14:23:28 GMT";i:3;s:15:"Server: gvs 1.0";}');
-
-foreach ($hs as $n) {
-    header("$n");
-}
-
 $now = time();
 $maxage = 365 * 24 * 3600;
+$fs=filesize($filename);
 
-foreach (array('Date: ' . gmdate('D, d M Y H:i:s') . ' GMT','Expires: ' . gmdate('D, d M Y H:i:s', $now + $maxage) . ' GMT','Cache-Control: public, max-age=' . $maxage) as $h)
-    header($h);
+$headers=array(
+'Content-Type: video/x-flv',
+'Content-Length: '.$fs,
+'Last-Modified: Wed, 22 Jun 2011 14:23:28 GMT',
+'Server: gvs 1.0',
+'Date: ' . gmdate('D, d M Y H:i:s') . ' GMT',
+'Expires: ' . gmdate('D, d M Y H:i:s',
+$now + $maxage) . ' GMT',
+'Cache-Control: public, max-age=' . $maxage
+);
+
+foreach ($headers as $h) {
+header($h);
+}
 
 while (!feof($fh)) {
 $buffer=fread($fh,131072);
